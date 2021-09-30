6b8537481f2a6e028f805137be726f463bf6b716
Rushabh12Shah <91649933+Rushabh12Shah@users.noreply.github.com>
2021-09-30 10:16:58 +0530

    Initial Commit for OKYC

---
+++ OKYC/OKYC/OKYC.h
@@ -0,0 +1,18 @@
+//
+//  OKYC.h
+//  OKYC
+//
+//  Created by Suraj Shandil on 9/30/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for OKYC.
+FOUNDATION_EXPORT double OKYCVersionNumber;
+
+//! Project version string for OKYC.
+FOUNDATION_EXPORT const unsigned char OKYCVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OKYC/PublicHeader.h>
+
+
